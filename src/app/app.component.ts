5060de57624e3381f08b9e55afb9f8c87b00d0c3
Abhilash-halcheeri <halcheeri0001abhi@gmail.com>
2023-07-12 14:34:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  api = 'https://api.whatsapp.com/send?phone=91';
   title = 'whatsapp';
+  numFromGroup: FormGroup;
+  unamePattern = "^[0-9]{10}$";
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.numFromGroup = this.fb.group({
+      mobile_number: ['', [Validators.required, Validators.pattern(this.unamePattern)]]
+    });
+  }
+
+  handleInput(event: any) {
+    event.preventDefault();
+    const value = event.target.value;
+    console.log(value)
+  }
+  onSubmit() {
+    console.log(this.numFromGroup)
+    if (this.numFromGroup.invalid) return;
+    const value = this.numFromGroup.value;
+    const mobileNumber = Number(value.mobile_number);
+    window.open(this.api+mobileNumber, '_blank')
+  }
+  get hasError(): string | null {
+    if (this.numFromGroup.controls['mobile_number'].errors) {
+      const errors = this.numFromGroup.controls['mobile_number'].errors;
+      if (errors['required']) {
+        return 'required';
+      } else if (errors['pattern']) {
+        return 'pattern';
+      }
+    }
+    return null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
